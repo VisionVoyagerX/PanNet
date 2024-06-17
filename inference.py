85dd99995a5c6f13d7f0c605c4862343ca16f6a0
ntantis <ntantis@intralot.com>
2024-06-17 13:20:53 +0300

    vis

--- inference.py
+++ inference.py
@@ -7,6 +7,7 @@ from torch.nn import MSELoss
 from torch.utils.data import DataLoader
 from torchvision.transforms import Resize, RandomHorizontalFlip, RandomVerticalFlip, RandomRotation
 from torchmetrics import MetricCollection, PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
+from torchmetrics.image import SpectralAngleMapper, ErrorRelativeGlobalDimensionlessSynthesis
 from torch.optim.lr_scheduler import StepLR
 from torchinfo import summary
 
@@ -14,7 +15,58 @@ from data_loader.DataLoader import DIV2K, GaoFen2, Sev2Mod, WV3, GaoFen2panforme
 from PanNet import PanNet_model
 from utils import *
 import numpy as np
+import time
 import matplotlib.pyplot as plt
+
+
+def measure_gpu_throughput(model, input1_, input2_):
+    input1 = input1_.to('cuda')
+    input2 = input2_.to('cuda')
+    model = model.to('cuda')
+
+    ave_forward_throughput = []
+
+    ave_start = time.time()
+    for t in range(300):
+        start = time.time()
+        x = model(input1, input2)
+        end = time.time()
+        fwd_throughput = 1/(end-start)
+        # print('forward_throughput is {:.4f}'.format(fwd_throughput))
+        ave_forward_throughput.append(fwd_throughput)
+
+    ave_fwd_throughput = np.mean(ave_forward_throughput[1:])
+
+    print('Mean throughput over 300 runs: {:.4f}'.format(ave_fwd_throughput))
+
+
+def measure_gpu_latency(model, input1_, input2_):
+    input1 = input1_.to('cuda')
+    input2 = input2_.to('cuda')
+    model = model.to('cuda')
+
+    repetitions = 300
+
+    # GPU-WARM-UP
+    for _ in range(20):  # Increase warm-up iterations to ensure GPU is fully warmed up
+        _ = model(input1, input2)
+        torch.cuda.synchronize()  # Ensure the warm-up operation completes
+
+    # Measure performance
+    timings = []
+    with torch.no_grad():
+        for _ in range(repetitions):
+            start = time.time()
+            _ = model(input1, input2)
+            torch.cuda.synchronize()  # Ensure the operation completes
+            end = time.time()
+            latency = end - start
+            timings.append(latency)
+
+    mean_latency = np.mean(timings)
+    print(
+        f"Mean time over {repetitions} runs: {mean_latency} seconds")
+    return mean_latency
 
 
 def main():
@@ -22,24 +74,22 @@ def main():
 
     if choose_dataset == 'GaoFen2':
         dataset = eval('GaoFen2')
-        tr_dir = '../pansharpenning_dataset/GF2/train/train_gf2.h5'
-        eval_dir = '../pansharpenning_dataset/GF2/val/valid_gf2.h5'
-        test_dir = '../pansharpenning_dataset/GF2/test/test_gf2_multiExm1.h5'
+        tr_dir = 'data/pansharpenning_dataset/GF2/train/train_gf2.h5'
+        eval_dir = 'data/pansharpenning_dataset/GF2/val/valid_gf2.h5'
+        test_dir = 'data/pansharpenning_dataset/GF2/test/test_gf2_multiExm1.h5'
         checkpoint_dir = 'checkpoints/pannet_model_GF2/pannet_model_2023_07_19-18_16_37.pth.tar'
         ms_channel = 4
         ergas_l = 4
     elif choose_dataset == 'WV3':
         dataset = eval('WV3')
-        tr_dir = '../pansharpenning_dataset/WV3/train/train_wv3.h5'
-        eval_dir = '../pansharpenning_dataset/WV3/val/valid_wv3.h5'
-        test_dir = '../pansharpenning_dataset/WV3/test/test_wv3_multiExm1.h5'
+        tr_dir = 'data/pansharpenning_dataset/WV3/train/train_wv3.h5'
+        eval_dir = 'data/pansharpenning_dataset/WV3/val/valid_wv3.h5'
+        test_dir = 'data/pansharpenning_dataset/WV3/test/test_wv3_multiExm1.h5'
         checkpoint_dir = 'checkpoints/pannet_model_WV3/pannet_model_WV3_2023_07_22-14_38_46.pth.tar'
         ms_channel = 8
         ergas_l = 4
     else:
         print(choose_dataset, ' does not exist')
-
-
     # Prepare device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
@@ -62,11 +112,10 @@ def main():
 
     # Initialize Model, optimizer, criterion and metrics
     model = PanNet_model(scale=4, ms_channels=ms_channel, mslr_mean=train_dataset.mslr_mean.to(device), mslr_std=train_dataset.mslr_std.to(device), pan_mean=train_dataset.pan_mean.to(device),
-                     pan_std=train_dataset.pan_std.to(device)).to(device)
+                         pan_std=train_dataset.pan_std.to(device)).to(device)
 
-
-    optimizer = SGD(model.parameters(), lr=0.000007, momentum=0.9, weight_decay=10e-7)
-
+    optimizer = SGD(model.parameters(), lr=0.000007,
+                    momentum=0.9, weight_decay=10e-7)
 
     '''# initialize optimizers
             if args.optim_type == 'adam':
@@ -79,17 +128,23 @@ def main():
 
     metric_collection = MetricCollection({
         'psnr': PeakSignalNoiseRatio().to(device),
-        'ssim': StructuralSimilarityIndexMeasure().to(device)
+        'ssim': StructuralSimilarityIndexMeasure().to(device),
+        'sam': SpectralAngleMapper().to(device),
+        'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
     })
 
     val_metric_collection = MetricCollection({
         'psnr': PeakSignalNoiseRatio().to(device),
-        'ssim': StructuralSimilarityIndexMeasure().to(device)
+        'ssim': StructuralSimilarityIndexMeasure().to(device),
+        'sam': SpectralAngleMapper().to(device),
+        'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
     })
 
     test_metric_collection = MetricCollection({
         'psnr': PeakSignalNoiseRatio().to(device),
-        'ssim': StructuralSimilarityIndexMeasure().to(device)
+        'ssim': StructuralSimilarityIndexMeasure().to(device),
+        'sam': SpectralAngleMapper().to(device),
+        'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
     })
 
     tr_report_loss = 0
@@ -102,27 +157,30 @@ def main():
     ergas_score = 0
     sam_score = 0
     q2n_score = 0
-    
+
     best_eval_psnr = 0
     best_test_psnr = 0
     current_daytime = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
     steps = 250000
     save_interval = 1000
     report_interval = 50
-    test_intervals = [20000, 40000, 60000, 80000, 1000000, 120000, 140000, 160000, 180000, 2000000, 2200000, 2400000, 2500000]
-    evaluation_interval = [20000, 40000, 60000, 80000, 1000000, 120000, 140000, 160000, 180000, 2000000, 2200000, 2400000, 2500000]
-    
+    test_intervals = [20000, 40000, 60000, 80000, 1000000, 120000,
+                      140000, 160000, 180000, 2000000, 2200000, 2400000, 2500000]
+    evaluation_interval = [20000, 40000, 60000, 80000, 1000000, 120000,
+                           140000, 160000, 180000, 2000000, 2200000, 2400000, 2500000]
+
     lr_decay_intervals = [100000, 200000]
     val_steps = 50
     continue_from_checkpoint = True
 
-    '''# Model summary
-    pan_example = torch.randn(
-        (1, 1, 256, 256)).to(device)
-    mslr_example = torch.randn(
-        (1, 4, 64, 64)).to(device)
+    input_tensor1 = torch.randn(
+        1, 1, 256, 256).to(device)  # Example input tensor 1
+    input_tensor2 = torch.randn(
+        1, ms_channel, 64, 64).to(device)  # Example input tensor 2
+    model.eval()
 
-    summary(model, pan_example, mslr_example, verbose=1)'''
+    measure_gpu_throughput(model, input_tensor1, input_tensor2)
+    measure_gpu_latency(model, input_tensor1, input_tensor2)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=0.1)
 
@@ -137,7 +195,7 @@ def main():
     def scaleMinMax(x):
         return ((x - np.nanmin(x)) / (np.nanmax(x) - np.nanmin(x)))
 
-    #WV3: 14
+    # WV3: 14
     # for gaofen: 15 or 8
     # evaluation mode
     model.eval()
@@ -152,10 +210,6 @@ def main():
             test_metric = test_metric_collection.forward(mssr, mshr)
             test_report_loss += test_loss
 
-            ergas_score += ergas_batch(mshr, mssr, ergas_l)
-            sam_score += sam_batch(mshr, mssr)
-            q2n_score += q2n_batch(mshr, mssr)
-
             figure, axis = plt.subplots(nrows=1, ncols=4, figsize=(15, 5))
             axis[0].imshow((scaleMinMax(mslr.permute(0, 3, 2, 1).detach().cpu()[
                             0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
@@ -163,7 +217,7 @@ def main():
             axis[0].axis("off")
 
             axis[1].imshow(pan.permute(0, 3, 2, 1).detach().cpu()[
-                            0, ...], cmap='gray')
+                0, ...], cmap='gray')
             axis[1].set_title('(b) PAN')
             axis[1].axis("off")
 
@@ -186,20 +240,19 @@ def main():
             gt = mshr.permute(0, 3, 2, 1).detach().cpu().numpy()
 
             np.savez(f'results/img_array_{choose_dataset}_{i}.npz', mslr=mslr,
-                        pan=pan, mssr=mssr, gt=gt)
+                     pan=pan, mssr=mssr, gt=gt)
 
         # compute metrics
         test_metric = test_metric_collection.compute()
         test_metric_collection.reset()
 
-
         # Print final scores
         print(f"Final scores:\n"
-            f"ERGAS: {ergas_score / (i+1)}\n"
-            f"SAM: {sam_score / (i+1)}\n"
-            f"Q2n: {q2n_score / (i+1)}\n"
-            f"PSNR: {test_metric['psnr'].item()}\n"
-            f"SSIM: {test_metric['ssim'].item()}")
+              f"ERGAS: {test_metric['ergas'].item()}\n"
+              f"SAM: {test_metric['sam'].item()}\n"
+              f"PSNR: {test_metric['psnr'].item()}\n"
+              f"SSIM: {test_metric['ssim'].item()}")
+
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import torch
 import datetime
 import numpy as np
 
+
 def get_checkpoint_path():
     """
     Returns a Path object pointing to the "configs" directory.
@@ -10,6 +11,7 @@ def get_checkpoint_path():
     current_dir = Path(__file__).resolve().parent
     checkpoints_path = current_dir / "checkpoints"
     return checkpoints_path
+
 
 def get_config_path():
     """
@@ -19,6 +21,7 @@ def get_config_path():
     config_path = current_dir / "configs"
     return config_path
 
+
 def get_data_loader_path():
     """
     Returns a Path object pointing to the "configs" directory.
@@ -26,6 +29,7 @@ def get_data_loader_path():
     current_dir = Path(__file__).resolve().parent
     data_loader_path = current_dir / "data_loader"
     return data_loader_path
+
 
 def get_model_path():
     """
@@ -35,15 +39,19 @@ def get_model_path():
     model_path = current_dir / "model"
     return model_path
 
+
 def save_checkpoint(state, filename: str, current_daytime: str):
-    #print("=> Saving checkpoint")
+    # print("=> Saving checkpoint")
     checkpoint_path = get_checkpoint_path()
 
     (checkpoint_path/filename).mkdir(parents=True, exist_ok=True)
-    torch.save(state,  checkpoint_path / filename / f'{filename}_{current_daytime}.pth.tar')
+    torch.save(state,  checkpoint_path / filename /
+               f'{filename}_{current_daytime}.pth.tar')
 
-def load_checkpoint(checkpoint, model, optimizer, tr_metrics, val_metrics, test_metrics): #test_metrics val_metrics
-    #print("=> Loading checkpoint")
+
+# test_metrics val_metrics
+def load_checkpoint(checkpoint, model, optimizer, tr_metrics, val_metrics, test_metrics):
+    # print("=> Loading checkpoint")
     try:
         model.load_state_dict(checkpoint['state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer'])
@@ -51,45 +59,7 @@ def load_checkpoint(checkpoint, model, optimizer, tr_metrics, val_metrics, test_
         val_metrics = checkpoint['val_metrics']
         test_metrics = checkpoint['test_metrics']
 
-        return (tr_metrics, val_metrics, test_metrics) #val_metrics
+        return (tr_metrics, val_metrics, test_metrics)  # val_metrics
     except:
         print('Could not load model')
         return Exception
-    
-
-
-def ergas_batch(reference_batch, synthesized_batch, scale_ratio):
-    reference_batch = reference_batch.cpu().numpy()
-    synthesized_batch = synthesized_batch.cpu().numpy()
-
-    n, h, w, c = reference_batch.shape
-    rmse = np.sqrt(np.mean((reference_batch - synthesized_batch) ** 2, axis=(1, 2)))
-    mean_ref = np.mean(reference_batch, axis=(1, 2))
-    ergas_values = 100 * scale_ratio * np.sqrt(np.mean((rmse / mean_ref) ** 2, axis=1))
-    return ergas_values
-
-
-def sam_batch(reference_batch, synthesized_batch):
-    reference_batch = reference_batch.cpu().numpy()
-    synthesized_batch = synthesized_batch.cpu().numpy()
-
-    product = np.sum(reference_batch * synthesized_batch, axis=3)
-    norm_ref = np.linalg.norm(reference_batch, axis=3)
-    norm_syn = np.linalg.norm(synthesized_batch, axis=3)
-    cos_theta = product / (norm_ref * norm_syn)
-    cos_theta = np.clip(cos_theta, -1, 1)  # Ensure the values are within [-1, 1]
-    sam_values = np.mean(np.arccos(cos_theta), axis=(1, 2))
-    return sam_values
-
-def q2n_batch(reference_batch, synthesized_batch):
-    reference_batch = reference_batch.cpu().numpy()
-    synthesized_batch = synthesized_batch.cpu().numpy()
-
-    mean_ref = np.mean(reference_batch, axis=(1, 2, 3))
-    mean_syn = np.mean(synthesized_batch, axis=(1, 2, 3))
-    var_ref = np.var(reference_batch, axis=(1, 2, 3))
-    var_syn = np.var(synthesized_batch, axis=(1, 2, 3))
-    covariance = np.mean((reference_batch - mean_ref[:, None, None, None]) * (synthesized_batch - mean_syn[:, None, None, None]), axis=(1, 2, 3))
-    
-    q2n_values = (4 * covariance * mean_ref * mean_syn) / ((var_ref + var_syn) * (mean_ref**2 + mean_syn**2))
-    return q2n_values
