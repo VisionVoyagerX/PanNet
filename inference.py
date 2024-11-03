0ee4a4a8f0c55a97faca3e7b0cc79880dd94f2ef
ntantis <ntantis@intralot.com>
2024-11-03 18:15:12 +0200

    Added MSE and RASE metrics

--- inference.py
+++ inference.py
@@ -7,8 +7,8 @@ from torch.nn import MSELoss
 from torch.utils.data import DataLoader
 from torchvision.transforms import Resize, RandomHorizontalFlip, RandomVerticalFlip, RandomRotation
 from torchmetrics import MetricCollection, PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
-from torchmetrics.image import SpectralAngleMapper, ErrorRelativeGlobalDimensionlessSynthesis
-from torch.optim.lr_scheduler import StepLR
+from torchmetrics.image import SpectralAngleMapper, ErrorRelativeGlobalDimensionlessSynthesis, RelativeAverageSpectralError, SpectralDistortionIndex
+from torchmetrics.regression import MeanSquaredError
 from torchinfo import summary
 
 from data_loader.DataLoader import DIV2K, GaoFen2, Sev2Mod, WV3, GaoFen2panformer
@@ -131,6 +131,8 @@ def main():
         'ssim': StructuralSimilarityIndexMeasure().to(device),
         'sam': SpectralAngleMapper().to(device),
         'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
+        'rase' : RelativeAverageSpectralError().to(device),
+        'mse' : MeanSquaredError().to(device),
     })
 
     val_metric_collection = MetricCollection({
@@ -138,6 +140,8 @@ def main():
         'ssim': StructuralSimilarityIndexMeasure().to(device),
         'sam': SpectralAngleMapper().to(device),
         'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
+        'rase' : RelativeAverageSpectralError().to(device),
+        'mse' : MeanSquaredError().to(device),
     })
 
     test_metric_collection = MetricCollection({
@@ -145,7 +149,11 @@ def main():
         'ssim': StructuralSimilarityIndexMeasure().to(device),
         'sam': SpectralAngleMapper().to(device),
         'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
+        'rase' : RelativeAverageSpectralError().to(device),
+        'mse' : MeanSquaredError().to(device),
     })
+
+    sdi_metric = SpectralDistortionIndex().to(device)
 
     tr_report_loss = 0
     val_report_loss = 0
@@ -153,6 +161,7 @@ def main():
     tr_metrics = []
     val_metrics = []
     test_metrics = []
+    sdi_results = []
 
     ergas_score = 0
     sam_score = 0
@@ -182,7 +191,7 @@ def main():
     measure_gpu_throughput(model, input_tensor1, input_tensor2)
     measure_gpu_latency(model, input_tensor1, input_tensor2)
 
-    scheduler = StepLR(optimizer, step_size=1, gamma=0.1)
+    # scheduler = StepLR(optimizer, step_size=1, gamma=0.1)
 
     step = 250000
 
@@ -209,6 +218,16 @@ def main():
             test_loss = criterion(mssr, mshr)
             test_metric = test_metric_collection.forward(mssr, mshr)
             test_report_loss += test_loss
+
+            # Normalize preds and target for SDI
+            # print(mssr.max())
+            preds_normalized = mssr / mssr.max()
+            target_normalized = mshr / mshr.max()
+
+            # Calculate SDI on normalized predictions and targets
+            sdi_value = sdi_metric(preds_normalized, target_normalized)
+            # print(sdi_value)
+            sdi_results.append(sdi_value.item())
 
             figure, axis = plt.subplots(nrows=1, ncols=4, figsize=(15, 5))
             axis[0].imshow((scaleMinMax(mslr.permute(0, 3, 2, 1).detach().cpu()[
@@ -246,12 +265,18 @@ def main():
         test_metric = test_metric_collection.compute()
         test_metric_collection.reset()
 
+        # Compute the average SDI
+        average_sdi = sum(sdi_results) / len(sdi_results)
+
         # Print final scores
         print(f"Final scores:\n"
               f"ERGAS: {test_metric['ergas'].item()}\n"
               f"SAM: {test_metric['sam'].item()}\n"
               f"PSNR: {test_metric['psnr'].item()}\n"
-              f"SSIM: {test_metric['ssim'].item()}")
+              f"SSIM: {test_metric['ssim'].item()}\n"
+              f"RASE: {test_metric['rase'].item()}\n"
+              f"MSE: {test_metric['mse'].item()}\n"
+              f"D_lambda: {average_sdi:.4f}")
 
 
 if __name__ == '__main__':
